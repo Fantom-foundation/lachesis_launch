d7860072b895704ea79a8f3dff668d4eb57f96bf
devintegral2 <devintegral2@sfxdx.ru>
2020-03-10 19:34:23 +1000

    stagingnet.toml

--- ubuntu/20.change-binary.sh
+++ ubuntu/20.change-binary.sh
@@ -9,8 +9,8 @@ cd ${SRC}/build/${NAME}
 # copy files to servers
 for i in $@; do
     ssh ${NAME}$i "mkdir -p /tmp/lachesis"
-    scp ./node$i/network.toml ${NAME}$i:/tmp/lachesis/
-    scp ./lachesis ${NAME}$i:/tmp/lachesis/
+    scp ./network.toml ${NAME}$i:/tmp/lachesis/
+    #scp ./lachesis ${NAME}$i:/tmp/lachesis/
 done
 
 # backup and replace
@@ -18,6 +18,6 @@ DT=$(date +"%Y-%m-%d")
 for i in $@; do
     ssh ${NAME}$i "sudo systemctl stop lachesis-node"
     ssh ${NAME}$i "test -d /tmp/lachesis-${DT} || sudo cp -r /home/lachesis /tmp/lachesis-${DT}"
-    ssh ${NAME}$i "sudo cp /tmp/lachesis/lachesis /tmp/lachesis/network.toml /home/lachesis/ && sudo chown -R lachesis:lachesis /home/lachesis"
+    ssh ${NAME}$i "sudo cp /tmp/lachesis/network.toml /home/lachesis/ && sudo chown -R lachesis:lachesis /home/lachesis"
     ssh ${NAME}$i "sudo systemctl start lachesis-node"
 done
