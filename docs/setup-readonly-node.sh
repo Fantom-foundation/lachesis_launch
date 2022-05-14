b860fb49975f160c4ec49227dfb5de4ff88bac66
Egor Lysenko <uprendis@gmail.com>
2022-05-15 00:04:28 +0400

    update version in setup-readonly-node.sh

--- docs/setup-readonly-node.sh
+++ docs/setup-readonly-node.sh
@@ -4,7 +4,7 @@
 # Bash script to launch a read-only go-opera node
 #######################################
 
-VERSION='feature/customizable-genesis-file'
+VERSION='release/1.1.1-rc.1'
 # Pick a genesis file for your network in ./genesis-files.md
 GENESIS='mainnet-109331-no-mpt.g'
 # snap or full
